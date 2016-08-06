24d201ebc25dd2d2d1b78c6edd10d85141cb9302
Subajo30 <subashine30@gmail.com>
2016-08-06 16:02:18 +0530

    Add files via upload

---
+++ JavaApplication2.java
@@ -0,0 +1,65 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author Student
+ */
+public class JavaApplication2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+  
+        
+    
+
+	private int applicantId;
+	private String applicantName;
+	private int yrsOfExp;
+	private String skills;
+	private double expectedSalary;
+	public JavaApplication2(int applicantId,String applicantName,int yrsOfExp, String skills, double expSalary){
+		
+	}
+	public int getApplicantId() {
+		return applicantId;
+	}
+	public void setApplicantId(int applicantId) {
+		this.applicantId = applicantId;
+	}
+	public String getApplicantName() {
+		return applicantName;
+	}
+	public void setApplicantName(String applicantName) {
+		this.applicantName = applicantName;
+	}
+	public int getYrsOfExp() {
+		return yrsOfExp;
+	}
+	public void setYrsOfExp(int yrsOfExp) {
+		this.yrsOfExp = yrsOfExp;
+	}
+	public String getSkills() {
+		return skills;
+	}
+	public void setSkills(String skills) {
+		this.skills = skills;
+	}
+	public double getExpectedSalary() {
+		return expectedSalary;
+	}
+	public void setExpectedSalary(double expectedSalary) {
+		this.expectedSalary = expectedSalary;
+	}
+
+
+}
+    
+
+
